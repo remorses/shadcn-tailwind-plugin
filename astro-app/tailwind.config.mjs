0489895c4f655574926dfbf690596a90cceb9369
remorses <beats.by.morse@gmail.com>
2024-12-13 12:20:45 +0100

    fix

--- astro-app/tailwind.config.mjs
+++ astro-app/tailwind.config.mjs
@@ -1,9 +1,10 @@
-import {shadcnPlugin} from '../dist/index.js'
+import shadcnPlugin from 'shadcn-tailwind-plugin'
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
         extend: {},
     },
-    plugins: [shadcnPlugin({})],
+    plugins: [shadcnPlugin({ lightColors: {} })],
 }
