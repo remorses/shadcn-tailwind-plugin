1957a088ba35875b0718d47ee8bc3defe7727b5c
remorses <beats.by.morse@gmail.com>
2024-12-13 18:20:44 +0100

    added shadcn

--- astro-app/tailwind.config.mjs
+++ astro-app/tailwind.config.mjs
@@ -2,9 +2,17 @@ import shadcnPlugin from 'shadcn-tailwind-plugin'
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: ['class'],
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
-        extend: {},
+    	extend: {
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		},
+    		colors: {}
+    	}
     },
-    plugins: [shadcnPlugin({ lightColors: {} })],
+    plugins: [shadcnPlugin({ lightColors: {} }), require("tailwindcss-animate")],
 }
